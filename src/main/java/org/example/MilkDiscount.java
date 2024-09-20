6aba7db05e34b20d205cf43650c030ca57a3e1d2
sebastianpiresmolin <piresmolinsecure@gmail.com>
2024-09-20 19:43:54 +0200

    Created MilkDiscount.

---
+++ src/main/java/org/example/MilkDiscount.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class MilkDiscount extends BaseDiscount {
+
+    public MilkDiscount(Discount nextDiscount) {
+        super(nextDiscount);
+    }
+
+    @Override
+    protected boolean isApplicable(Product product) {
+        return product.getName().equalsIgnoreCase("mjölk");
+    }
+
+    @Override
+    protected double calculateDiscount(Product product) {
+        return product.getPrice() * 0.05;
+    }
+
+    @Override
+    public String getDescription(Product product, Object additionalInfo) {
+        return "5% Rabatt på mjölk";
+    }
+}
